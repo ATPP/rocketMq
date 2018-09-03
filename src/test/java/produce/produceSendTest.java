69bbda353c6c54866b605433dd34844aa87b071d
wanghong <1198394709@qq.com>
2018-09-03 21:41:38 +0800

    produce+consumer

--- src/test/java/produce/produceSendTest.java
+++ src/test/java/produce/produceSendTest.java
@@ -1,13 +1,37 @@
 package produce;
 
+import com.hom.common.consumer.RocketMqConsumer;
 import com.hom.common.produce.RocketMqProduce;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class produceSendTest {
-    private RocketMqProduce rocketMqProduce = new RocketMqProduce();
+    private ApplicationContext container;
+
+    @Before
+    public void setup() {
+        container = new ClassPathXmlApplicationContext("classpath:spring-producer.xml");
+    }
 
     @Test
-    public void send(){
-        rocketMqProduce.init();
+    public void sendMessage() throws Exception {
+        RocketMqProduce producer = container.getBean(RocketMqProduce.class);
+        for (int i = 0; i < 20; i++) {
+            //创建一条消息对象，指定其主题、标签和消息内容
+            Message msg = new Message(
+                    "diffebnTopic",
+                    null,
+                    ("Spring RocketMQ demo " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* 消息内容 */
+            );
+            //发送消息并返回结果
+            SendResult result = producer.getProducer().send(msg);
+//            producer.send("diffebnTopic","消息"+i);
+//            System.out.printf("%s%n", sendResult);
+        }
     }
 }
